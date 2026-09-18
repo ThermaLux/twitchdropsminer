f3e38f09daecb22027d2d0eeb9ca3025ec3ea0d5
ThermaLux <101374429+ThermaLux@users.noreply.github.com>
2026-09-18 15:10:20 -0400

    Change client type from ANDROID_APP to SMARTBOX

--- src/core/client.py
+++ src/core/client.py
@@ -71,7 +71,7 @@ class Twitch:
         self._claim_finalized_drops: set[str] = set()
         self._mnt_triggers: deque[datetime] = deque()
         # Client type and auth
-        self._client_type: ClientInfo = ClientType.ANDROID_APP
+        self._client_type: ClientInfo = ClientType.SMARTBOX
         self._auth_state: _AuthState = _AuthState(self)
         # GUI (will be set by main.py)
         self.gui: WebGUIManager = None  # type: ignore[assignment]
